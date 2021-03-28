b5284e4e97b3327a12502e1998d3f7e6672e4fa1
Ilnyr Nasybullin <ilnur.nasybullin2000@yandex.ru>
2021-03-28 19:39:36 +0300

    rename realEndTime & replace Set on List

--- Project/src/main/java/app/airlines/domain/Flight.java
+++ Project/src/main/java/app/airlines/domain/Flight.java
@@ -3,7 +3,6 @@ package app.airlines.domain;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 public class Flight extends AbstractEntity {
